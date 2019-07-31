a212135964d1c0c617f6d63e20db5b4ffa1123a2
Firehiwot1 <fmazengia@gmail.com>
2019-07-31 16:24:05 -0400

    some adj

---
+++ src/main/java/com/example/demo/HomeControllerCloudinary.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+import com.cloudinary.Singleton;
+import com.cloudinary.Transformation;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class HomeControllerCloudinary {
+
+    @Autowired
+    MessageRepository messagesRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+      @Autowired
+    UserService userService;
+
+    @RequestMapping("/")
+    public String listMessages(Model model) {
+        model.addAttribute("messages", messagesRepository.findAll());
+        if (userService.getUser() != null) {
+            model.addAttribute("user_id", userService.getUser().getId());
+        }
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String messageform(Model model) {
+        model.addAttribute("bullhorn", new Bullhorn());
+        return "messageform";
+    }
+
+
+    @PostMapping("/add")
+
+    public String processmessage(@ModelAttribute Bullhorn bullhorn,
+                                 @RequestParam("file") MultipartFile file)
+
+    {
+        if (file.isEmpty()) {
+            return "redirect;/add";
+        }
+        try
+
+        {
+            Map uploadResult = cloudc.upload(file.getBytes(),
+                    ObjectUtils.asMap("resourcetype", "auto"));
+            //bullhorn.setHeadshot(uploadResult.get("url").toString());
+            messagesRepository.save(bullhorn);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "redirect:/add";
+
+        }
+        return "redirect:/";
+    }
+}
