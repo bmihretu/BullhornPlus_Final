c778417d5edf0b032446f07bf148e0e02ef2da64
Firehiwot1 <51675215+Firehiwot1@users.noreply.github.com>
2019-08-01 07:42:15 -0400

    Update HomeControllerCloudinary.java

--- src/main/java/com/example/demo/HomeControllerCloudinary.java
+++ src/main/java/com/example/demo/HomeControllerCloudinary.java
@@ -1,92 +1,177 @@
 package com.example.demo;
-import com.cloudinary.Cloudinary;
-import com.cloudinary.Singleton;
-import com.cloudinary.Transformation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+package com.example.demo.web.controller;
 
+import com.cloudinary.utils.ObjectUtils;
+import com.example.demo.business.entities.Message;
+import com.example.demo.business.entities.User;
+import com.example.demo.business.entities.repositories.MessageRepository;
+import com.example.demo.business.entities.repositories.UserRepository;
+import com.example.demo.business.services.CloudinaryConfig;
+import com.example.demo.business.services.CustomerUserDetails;
+import com.example.demo.business.services.UserService;
+import com.example.demo.business.util.MD5Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Map;
 
-@Component
-public class CloudinaryConfig {
-    private Cloudinary cloudinary;
+@Controller
+public class HomeController {
+    @Autowired
+    MessageRepository messageRepository;
 
     @Autowired
-    public CloudinaryConfig(@Value("${cloud.key}") String key,
-                            @Value("${cloud.secret}") String secret,
-                            @Value("${cloud.name}") String cloud) {
-        cloudinary = Singleton.getCloudinary();
-        cloudinary.config.cloudName = cloud;
-        cloudinary.config.apiSecret = secret;
-        cloudinary.config.apiKey = key;
+    UserRepository userRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+
+    @Autowired
+    UserService userService;
+
+    //Users with Admin role can view this page
+    @RequestMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("mD5Util", new MD5Util());
+        return "admin";
     }
 
-    public Map upload(Object file, Map options) {
-        try {
-            return cloudinary.uploader().upload(file, options);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+    @RequestMapping("/")
+    public String listMessages(Model model) {
+        model.addAttribute("messages", messageRepository.findAllByOrderByPostedDateTimeDesc());//generate select * statement
+        //we need because the below statement wont run if there is no authenticate user
+        if (userService.getUser() != null) {
+            model.addAttribute("user", userService.getUser());
+//            model.addAttribute("HASH", MD5Util.md5Hex(userService.getUser().getEmail()));
         }
+        model.addAttribute("mD5Util", new MD5Util());
+        return "list";
     }
 
-    /**
-     * This method generates the URL for the actor's list
-     */
-    public String createUrl(String name) {
-        return cloudinary
-                .url()
-                .transformation(new Transformation()
-                        .width(100)
-                        .height(100)
-                        .crop("fill")
-                        .radius(50)
-                        .gravity("face"))
-                .generate(name);
+    @GetMapping("/add")
+    public String messageForm(Principal principal, Model model) {
+        model.addAttribute("imageLabel", "Upload Image");
+        model.addAttribute("user", userService.getUser());
+        model.addAttribute("message", new Message());
+        return "messageform";
     }
 
-    /**
-     * This method generates the URL for an image whose name is known and has been provided
-     */
-    public String createUrl(String name, int width, int height) {
-        return cloudinary
-                .url()
-                .transformation(new Transformation()
-                        .width(width)
-                        .height(height)
-                        .crop("fill")
-                        .radius(50)
-                        .gravity("face"))
-                .generate(name);
+    @PostMapping("/process")
+    public String processForm(@Valid @ModelAttribute("message") Message message,
+                              BindingResult result,
+                              @RequestParam("file") MultipartFile file,
+                              Model model) {
+        model.addAttribute("imageLabel", "Upload Image");
+        model.addAttribute("user", userService.getUser());
+        //check for errors on the form
+        if (result.hasErrors()) {
+            for (ObjectError e : result.getAllErrors()) {
+                System.out.println(e);
+            }
+            return "messageform";
+        }
 
+        if (!file.isEmpty()) {
+            try {
+                Map uploadResult = cloudc.upload(
+                        file.getBytes(), ObjectUtils.asMap("resourcetype", "auto"));
+                String url = uploadResult.get("url").toString();
+                String uploadedName = uploadResult.get("public_id").toString();
+                String transformedImage = cloudc.createUrl(uploadedName, 150, 150);
+                message.setPicturePath(transformedImage);
+                message.setUser(userService.getUser());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "redirect:/add";
+            }
+        } else {
+            //if file is empty and there is a picture path then save item
+            if (message.getPicturePath().isEmpty()) {
+                return "messageform";
+            }
+        }
+        messageRepository.save(message);
+        return "redirect:/";
     }
 
-    /**
-     * Creates a transformation from the URL provided
-     */
-    public String createSmallImage(String url, int width, int height) {
-        return cloudinary
-                .url()
-                .transformation(new Transformation()
-                        .width(width)
-                        .height(height)
-                        .crop("fill")
-                        .radius(50)
-                        .gravity("face"))
-                .type("fetch").generate(url);
-
+    @RequestMapping("/detail/{id}")
+    public String showMessage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        if (userService.getUser() != null) {
+            model.addAttribute("user_id", userService.getUser().getId());
+        }
+        return "show";
     }
 
-    public String createUrl(String name, int width, int height, String action) {
-        return cloudinary
-                .url()
-                .transformation(new Transformation()
-                        .width(width) //50
-                        .height(height)//50
-                        .border("2px_solid_black")
-                        .crop(action)) //thumb
-                .imageTag(name);//my message
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        if (userService.getUser() != null) {
+            model.addAttribute("user", userService.getUser());
+        }
+        return "messageform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteMessage(@PathVariable("id") long id) {
+        messageRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/check")
+    public String check(@RequestParam("check") long[] ids,
+                        Model model) {
+        for (long id : ids) {
+            messageRepository.deleteById(id);
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/about")
+    public String getAbout() {
+        return "about";
+    }
+
+    @RequestMapping("/myprofile")
+    public String getProfile(Principal principal, Model model) {
+        User user = userService.getUser();
+        model.addAttribute("user", user);
+        model.addAttribute("myuser", user);
+        model.addAttribute("HASH", MD5Util.md5Hex(user.getEmail()));
+        return "profile";
+    }
+
+    @RequestMapping("/user/{id}")
+    public String getUser(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id).get();
+        model.addAttribute("user", user);
+        model.addAttribute("myuser", userService.getUser());
+        model.addAttribute("HASH", MD5Util.md5Hex(user.getEmail())); //save every person email as hash
+        return "profile";
+    }
+
+    //AUXILLARY FUNCTION!!!
+    //Use the below code INSIDE METHOD to pass user into view
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model) {
+
+        User myuser = ((CustomerUserDetails)
+                ((UsernamePasswordAuthenticationToken) principal)
+                        .getPrincipal())
+                .getUser();
+        //or
+        myuser = userService.getUser();
+        model.addAttribute("myuser", myuser);
+        return "secure";
     }
 }
